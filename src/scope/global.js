b1ea2f36c801ec41f0e4d022affe5bee842d8526
Noith <nimoba101@gmail.com>
2022-07-06 16:45:20 -0500

    scope global

---
+++ src/scope/global.js
@@ -0,0 +1,14 @@
+//Variables
+var a; //Declarar
+var b = 'b'; // Declarar y aseignar
+b = 'bb'; //Reasignar
+var a = 'aa'; // Redeclarar
+
+//Global Scope
+var fruit = 'Apple';
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
